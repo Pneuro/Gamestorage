c2ff58281a350292ccea2cacaa486125eb11ff8a
Pneuro <57775304+Pneuro@users.noreply.github.com>
2020-01-17 09:05:19 -0500

    enter

---
+++ snake_practice.py
@@ -0,0 +1,40 @@
+##### This app draws a heart.
+
+import turtle as t
+import random
+    
+t.color('red')
+t.bgcolor('black')
+t.speed(10)
+while True:
+    
+    
+    colors = ['red', 'green', 'blue', 'white', 'magenta', 'yellow']
+    
+    
+    change=random.choice(colors)
+    
+    
+    def draw():
+        t.color(change)
+        t.lt(45)
+        t.forward(100)
+        t.circle(50, 180, 25)
+        t.rt(90)
+        t.circle(50, 180, 25)
+        t.forward(100)
+    
+
+    def next():
+        t.color(change)
+        t.lt(45)
+        t.circle(90)
+
+    def fill():
+        t.fd(1)
+        t.rt(1)
+    
+
+    draw()
+    next()
+    fill()
